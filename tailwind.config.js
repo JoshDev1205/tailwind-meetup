95d05bdfb49a7ee6d924e20988e8c15157969feb
Joseph <jospina@zetech.com.ar>
2019-11-21 18:24:03 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'vue-green': '#41B883',
+        'react-blue': '#61DBFB',
+        'angular-red': '#b52e31'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
